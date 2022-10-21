ae6cb34ea8bb47234b593eb0d375cb293d6471e5
Gasso21 <jhjung1227@naver.com>
2022-10-22 00:03:53 +0900

    edit

--- Project/Gcova.py
+++ Project/Gcova.py
@@ -1,1 +1,7 @@
-main
+from urllib.parse import urlencode, unquote
+from urllib.request import Request, urlopen
+import xml.etree.ElementTree as ET
+
+from module import BusNo2BusId
+busRouteId = BusNo2BusId.getBusRouteList(506)
+print(busRouteId)
--- Project/module/BusNo2BusId.py
+++ Project/module/BusNo2BusId.py
@@ -1,1 +1,33 @@
-text
+def getBusRouteList(strSrch):
+    from urllib.parse import urlencode, unquote
+    from urllib.request import Request, urlopen
+    import xml.etree.ElementTree as ET
+    
+    #API URL & Key
+    url = 'http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList'
+    ServiceKey = unquote('gjPavFovnjVJ7CFP7V%2FeNq3fwMNiW4GVn%2FNDcWSTJpqb1uII9BrKZJ8Vq2vZmYbm3ahPDMhFY7pF5e7jcfwy7w%3D%3D')
+
+    #Parameters
+    params = {}
+    params['ServiceKey'] = ServiceKey
+    params['strSrch'] = strSrch
+    
+    #Request
+    queryParams = '?' + urlencode(params)
+    request = Request(url + queryParams)
+    #request.get_method = lambda: 'GET'
+
+    #parsing
+    doc = ET.parse(urlopen(request))
+    
+    root = doc.getroot()
+    msgBody    = root.find("msgBody")
+    itemList   = msgBody.find("itemList")
+    busRouteId = itemList.find("busRouteId").text
+
+    return busRouteId
+
+if __name__ == "__main__":
+    bus_number = input("버스 번호를 입력하시오: \n")
+    bus_Id = getBusRouteList(bus_number)
+    print("Bus Route ID : ",bus_Id)
